5cf5256cbddee0ba1f362604facb5b40db436e28
Red <saraalva@usc.edu>
2022-07-20 21:56:42 -0700

    it works

--- cell.js
+++ cell.js
@@ -1,18 +1,25 @@
 //cells on the board
 class Cell{
 
+  static SIZE = 50;
+
   constructor(x,y,isAlive){
-    this.x = x;
-    this.y = y;
+    this.x = x; //also left side of square
+    this.y = y; //also top of square
     this.isAlive = isAlive;
+    //right side edge of square
+    this.right = x+Cell.SIZE;
+    //bottom edge of square
+    this.bottom = y+Cell.SIZE;
+    this.neighbors = 0;
   }
 
   draw(){
     if(this.isAlive == true){
-      drawRect(this.x,this.y,20,20, "red");
+      drawRect(this.x,this.y,Cell.SIZE,Cell.SIZE, "red");
     }
     else{
-      drawRect(this.x,this.y,20,20, "grey");
+      drawRect(this.x,this.y,Cell.SIZE,Cell.SIZE, "grey");
     }
   }
 
@@ -25,7 +32,6 @@ class Cell{
       this.isAlive = false;
     }
   }
-
 
 
 }
--- main.js
+++ main.js
@@ -1,31 +1,139 @@
-console.log("hello world");
 
-var button = document.getElementById('goButton');
-var ily="";
-var paragraph = document.getElementById('input');
-var color = ["red", "blue", "yellow", "pink", "orange"];
-var i = 0;
 var canvy = document.getElementById('game');
-
-//color changing inputs
-function buttonClick(){
-  ily = window.prompt("What do you want to type?");
-  console.log(ily);
-  paragraph.textContent = ily;
-  paragraph.style.color = color[i];
-  i++;
-  if(i>=color.length){
-    i = 0;
+//click cells to change status
+function clickCells(event){
+  for(var j = 0; j < rows; j++){
+    for(var i = 0; i < columns; i++){
+      var cell = grid[j][i];
+      if(event.layerX > cell.x
+        && event.layerX < cell.right
+        && event.layerY > cell.y
+        && event.layerY < cell.bottom){
+          cell.changeAlive();
+          cell.draw();
+        }
+    }
   }
 }
-function canvasClick(event){
-  var cel = new Cell(event.layerX, event.layerY, true);
-  if(event.buttons == 1){
-    cel.draw();
+//grid maker
+//columns
+var columns = board.width/Cell.SIZE;
+//rows
+var rows = board.height/Cell.SIZE;
+//grid array
+var grid = [];
+var row = [];
+function makeGrid(){
+  var x = 0;
+  var y = 0;
+  for(var j = 0; j < rows; j++){
+    for(var i = 0; i < columns; i++){
+      var cell = new Cell(x, y, false);
+      row.push(cell);
+      cell.draw();
+      x += Cell.SIZE;
+    }
+    grid.push(row);
+    row = [];
+    y += Cell.SIZE;
+    x = 0;
   }
-
 }
+makeGrid();
 
+//check neighbors, change status accordingly
+function checkNeighbors(){
+  for(var j = 0; j < rows; j++){
+    for(var i = 0; i < columns; i++){
+      var cell = grid[j][i];
+      //check to make sure its possible lul
+      //cell above
+      if(j-1 >= 0){
+        if(grid[j-1][i].isAlive){
+          cell.neighbors++;
+        }
+      }
+      //cell below
+      if(j+1 < rows){
+        if(grid[j+1][i].isAlive){
+          cell.neighbors++;
+        }
+      }
+      //cell to the left
+      if(i-1 >= 0){
+        if(grid[j][i-1].isAlive){
+          cell.neighbors++;
+        }
+      }
+      //cell to the right
+      if(i+1 < columns){
+        if(grid[j][i+1].isAlive){
+          cell.neighbors++;
+        }
+      }
+      //diagonal top left cell
+      if(j-1 >= 0 && i-1 >= 0){
+        if(grid[j-1][i-1].isAlive){
+          cell.neighbors++;
+        }
+      }
+      //diagonal top right cell
+      if(j-1 >= 0 && i+1 < columns){
+        if(grid[j-1][i+1].isAlive){
+          cell.neighbors++;
+        }
+      }
+      //diagonal bottom left cell
+      if(j+1 < rows && i-1 >= 0){
+        if(grid[j+1][i-1].isAlive){
+          cell.neighbors++;
+        }
+      }
+      //diagonal bottom right cell
+      if(j+1 < rows && i+1 < columns){
+        if(grid[j+1][i+1].isAlive){
+          cell.neighbors++;
+        }
+      }
+    }
+  }
+}
+function changeState(){
+  for(var j = 0; j < rows; j++){
+    for(var i = 0; i < columns; i++){
+      var cell = grid[j][i];
+      //change board depending on number of num neighbors
+      //if the cell is living and has more than 3 or less than 2 neighbors,
+      //it dies
+      if(cell.isAlive && cell.neighbors < 2){
+        cell.changeAlive();
+      }
+      else if(cell.isAlive && cell.neighbors > 3){
+        cell.changeAlive();
+      }
+      //if cell is dead and has 3 neighbors, it becomes alive
+      else if(!cell.isAlive && (cell.neighbors == 3)){
+        cell.changeAlive();
+      }
+      cell.neighbors = 0;
+    }
+  }
+}
+//once neighbors are checked, update the game board with new values
+var gen = 0;
+function generation(){
+  checkNeighbors();
+  changeState();
+  //draw updated grid
+  for(var j = 0; j < rows; j++){
+    for(var i = 0; i < columns; i++){
+      var cell = grid[j][i];
+      cell.draw();
+    }
+  }
+  gen++;
+}
+//i love you beck
 //events
-canvy.addEventListener("mousemove", canvasClick);
-button.addEventListener("click", buttonClick);
+canvy.addEventListener("click", clickCells);
+setInterval(generation,2000);
