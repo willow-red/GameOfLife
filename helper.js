c598d3b822a2a54a66ce95ce0b1415bd471444c6
Red <saraalva@usc.edu>
2022-07-23 15:47:24 -0700

    window

--- helper.js
+++ helper.js
@@ -2,6 +2,9 @@
 var board = document.getElementById('game');
 var drawer = board.getContext("2d");
 
+board.width = window.innerWidth;
+board.height = window.innerHeight;
+
 function drawRect(x, y, width, height, color){
   drawer.beginPath();
   drawer.rect(x, y, width, height);
